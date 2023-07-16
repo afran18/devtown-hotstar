9f9ae2a1073b0bfd63ea640660b078a0f5ff0ca8
afran.18 <afran.vk18@gmail.com>
2023-07-16 23:55:42 +0530

    Top SLider Done JS started & Data.js Created

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Loki',
+        des: 'Loki, the God of Mischief, steps out of his brothers shadow to embark on an adventure that takes place after the events of Avengers: Endgame',
+        image:'res\\images\\slider 1.PNG'
+    },
+    {
+        name: 'Falcon and the Winter Soldier',
+        des: 'Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience.',
+        image:'res\\images\\slider 2.PNG'
+    },
+    {
+        name: "Wanda Vision",
+        des: "Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
+        image:"res\\images\\slider 3.PNG"
+    },
+    {
+        name: 'Raya and the Last Dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
+        image: 'res\\images\\slider 4.PNG'
+    },
+    {
+        name: 'Luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply held secret: he is a sea monster from another world just below the water surface.',
+        image: 'res\\images\\slider 5.PNG'
+    }
+]
---
+++ index.js
@@ -0,0 +1,56 @@
+
+const carousel = document.querySelector('.carousel');
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+    if(slideIndex >= movies.length){
+        slideIndex = 0;
+    }
+
+    //creating DOM elements
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    //attaching all elements
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(imgElement);
+    slide.appendChild(content);
+    carousel.appendChild(slide);
+
+    //setting up images
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+
+    //setting elements classnames
+    slide.className = 'slider';
+    content.className = 'slide-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-des';
+    imgElement.className = 'slider-img';
+
+    sliders.push(slide);
+
+    if(sliders.length)
+    {
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30*(sliders.length - 2)}px)`;
+    }
+
+}
+
+//call sliders func
+for(let i = 0; i<3; i++){
+    createSlide();
+}
+
+setInterval(() => {
+    createSlide();
+}, 3000);
